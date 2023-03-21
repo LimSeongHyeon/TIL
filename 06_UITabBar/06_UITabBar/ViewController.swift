73e225442ce2e492f4ded5e561a5b23cdfde4737
LimSeongHyeon <shot7351@gmail.com>
2023-03-21 18:26:00 +0900

    init project

---
+++ 06_UITabBar/06_UITabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  06_UITabBar
+//
+//  Created by Lim on 2023/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
